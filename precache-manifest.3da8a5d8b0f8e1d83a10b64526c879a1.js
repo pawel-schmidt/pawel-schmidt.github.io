2a8040e74bfe28ca04fbcf8a9fa60d3f1411edb5
Pawel Schmidt <px.sxxx@gmail.com>
2018-09-20 08:43:57 +0200

    Initial commit

---
+++ precache-manifest.3da8a5d8b0f8e1d83a10b64526c879a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "fee221c8466937447b11",
+    "url": "/js/chunk-vendors.b85a3217.js"
+  },
+  {
+    "revision": "23621dc3375ed54751e7",
+    "url": "/js/app.d31dd426.js"
+  },
+  {
+    "revision": "bfe70eed5a51c6e16e9bbf068d3fa0bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fee221c8466937447b11",
+    "url": "/css/chunk-vendors.e7222bb5.css"
+  }
+];
